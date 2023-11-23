e56ff6743ca5d6313c3ed98ca2695b3d11675512
Ilya <pankratovilyasergeevich@gmail.com>
2023-11-23 02:18:37 +0200

    Added 2 funcs

--- sortr.py
+++ sortr.py
@@ -1,3 +1,6 @@
+import os
+import re
+
 # TODO: 1) Open settings.txt file to get hot folder's paths.
 # TODO: -- All input files go there.
 
@@ -22,16 +25,28 @@ except IndexError:
 # TODO: 2) Request to user to get path who contains files to do.
 from_path = input().strip()
 
+
 # TODO: 3) Read all filenames in from_path directory and sort each file to their hot folder.
 # TODO: -- Using file size, colorify and canvas print size.
 # TODO: !!! DO RIGHT FILENAME PATTERN !!!
-right_filename_pattern = r'(?i)(?P<date>\d{2}-\d{2}).*?' \
-                         r'(?P<OrderID>\d+)_.*?' \
-                         r'(?P<size>\d+[xх]\d+)_.*?' \
-                         r'(?P<color>\d\+\d)_.*?' \
-                         r'(?P<canvas_size>[\w]{3}\d\+?)_.*?' \
-                         r'(?P<quantity>[\d ]{1,}).*?' \
-                         r'(?P<file_format>\.pdf)?'
+
+
+def get_all_filenames_in_directory(path: str) -> list[str]:
+    for _, _, filenames in os.walk(from_path):
+        return filenames
+
+
+def get_params_from_filename(filename: str) -> list[str]:
+    right_filename_pattern = r'(?i)(?P<date>\d{2}-\d{2}).*?' \
+                             r'(?P<OrderID>\d+)_.*?' \
+                             r'(?P<size>\d+[xх]\d+)_.*?' \
+                             r'(?P<color>\d\+\d)_.*?' \
+                             r'(?P<canvas_size>[\w]{3}\d\+?)_.*?' \
+                             r'(?P<quantity>[\d ]{1,}).*?' \
+                             r'(?P<file_format>\.pdf)?'
+
+    _, _, file_size, file_colorify, file_canvas_print_size, _, _ = re.findall(right_filename_pattern, filename)[0]
+    return [file_size, file_colorify, file_canvas_print_size]
 
 # TODO: ??? 4) Check output hot folder "output" and "errors" and send a message to user how many files are done
 #  len(output) and how many files are invalid len(errors). summ() of this file will == to len(from_path).
