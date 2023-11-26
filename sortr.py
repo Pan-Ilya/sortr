26ee63fe7e12bfa0c94dd6665548bb27a942dd4a
Ilya <pankratovilyasergeevich@gmail.com>
2023-11-26 20:52:30 +0200

    Using 'extra' params from filename in match/case statement.

--- sortr.py
+++ sortr.py
@@ -42,8 +42,8 @@ except FileNotFoundError:
     exit_program(5, 1)
 
 
-# TODO: 3) Read all filenames in from_path directory and sort each file to their hot folder.
-# TODO: -- Using file size, colorify and canvas print size.
+# 3) Read all filenames in from_path directory and sort each file to their hot folder.
+# -- Using file size, colorify and canvas print size.
 
 
 def get_all_filenames_in_directory(path: str) -> list[str]:
@@ -58,7 +58,7 @@ def get_params_from_filename(filename: str) -> list[str]:
     right_filename_pattern = r'(?i).*?(?P<size>\d+[xх]\d+).*?' \
                              r'(?P<color>\d\+\d).*?' \
                              r'(?P<canvas_size>SRA\d\+?).*?' \
-                             r'(?P<extra>--)?\.'
+                             r'(?P<extra>--)?(?:$|\.)'
 
     file_size, file_colorify, file_canvas_print_siz, extra = re.findall(right_filename_pattern, filename)[0]
     return [file_size, file_colorify, file_canvas_print_siz, extra]
@@ -72,28 +72,40 @@ while True:
 
             match filename_params:
                 # Всё что идёт в viz_4+0
-                case f_size, f_colorify, f_canvas_print_size \
+                case f_size, f_colorify, f_canvas_print_size, _ \
                     if f_size in ['89x49', '49x89'] and f_colorify in ['4+0', '1+0'] and f_canvas_print_size == 'SRA3':
                     os.replace(filename, input_viz_4_0 + filename)
 
                 # Всё что идёт в viz_4+4
-                case f_size, f_colorify, f_canvas_print_size \
+                case f_size, f_colorify, f_canvas_print_size, _ \
                     if f_size in ['89x49', '49x89'] and f_colorify in ['4+4', '1+1'] and f_canvas_print_size == 'SRA3':
                     os.replace(filename, input_viz_4_4 + filename)
 
                 # Всё что идёт сразу в папку "готово" - output
-                case f_size, f_colorify, f_canvas_print_size \
+                case f_size, f_colorify, f_canvas_print_size, _ \
                     if (f_size in ['487x330', '330x487'] and f_canvas_print_size == 'SRA3+') or \
                        (f_size in ['450x320', '320x450'] and f_canvas_print_size == 'SRA3'):
                     os.replace(filename, output + filename)
 
+                # Всё что идёт в SRA3_universal_1_rez
+                case f_size, f_colorify, f_canvas_print_size, extra \
+                    if f_size not in ['450x320', '320x450', '487x330', '330x487'] and \
+                       f_canvas_print_size == 'SRA3' and extra:
+                    os.replace(filename, input_SRA3_1_rez + filename)
+
+                # Всё что идёт в SRA3+_universal_1_rez
+                case f_size, f_colorify, f_canvas_print_size, extra \
+                    if f_size not in ['450x320', '320x450', '487x330', '330x487'] and \
+                       f_canvas_print_size == 'SRA3+' and extra:
+                    os.replace(filename, input_SRA3_plus_1_rez + filename)
+
                 # Всё что идёт в SRA3_universal
-                case f_size, f_colorify, f_canvas_print_size \
+                case f_size, f_colorify, f_canvas_print_size, _ \
                     if f_size not in ['450x320', '320x450', '487x330', '330x487'] and f_canvas_print_size == 'SRA3':
                     os.replace(filename, input_SRA3 + filename)
 
                 # Всё что идёт в SRA3+_universal
-                case f_size, f_colorify, f_canvas_print_size \
+                case f_size, f_colorify, f_canvas_print_size, _ \
                     if f_size not in ['450x320', '320x450', '487x330', '330x487'] and f_canvas_print_size == 'SRA3+':
                     os.replace(filename, input_SRA3_plus + filename)
 
@@ -103,7 +115,7 @@ while True:
 
         time.sleep(3)
 
-    except:
+    except NotADirectoryError:
         print(f'Не понимаю файл с именем {filename}\n направляю в папку с ошибками')
         os.replace(filename, errors + filename)
 
