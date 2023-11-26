b86c6a03b7ee3228b2e659db9e5eff75030f832b
Ilya <pankratovilyasergeevich@gmail.com>
2023-11-26 18:27:00 +0200

    Refactor main loop

--- sortr.py
+++ sortr.py
@@ -64,41 +64,46 @@ def get_params_from_filename(filename: str) -> list[str]:
 
 while True:
 
-    for filename in get_all_filenames_in_directory(from_path):
-        filename_params = get_params_from_filename(filename)
+    try:
+        for filename in get_all_filenames_in_directory(from_path):
+            filename_params = get_params_from_filename(filename)
 
-        match filename_params:
-            # Всё что идёт в viz_4+0
-            case f_size, f_colorify, f_canvas_print_size \
-                if f_size in ['89x49', '49x89'] and f_colorify == '4+0' and f_canvas_print_size == 'SRA3':
-                os.replace(filename, input_viz_4_0 + filename)
+            match filename_params:
+                # Всё что идёт в viz_4+0
+                case f_size, f_colorify, f_canvas_print_size \
+                    if f_size in ['89x49', '49x89'] and f_colorify in ['4+0', '1+0'] and f_canvas_print_size == 'SRA3':
+                    os.replace(filename, input_viz_4_0 + filename)
 
-            # Всё что идёт в viz_4+4
-            case f_size, f_colorify, f_canvas_print_size \
-                if f_size in ['89x49', '49x89'] and f_colorify == '4+4' and f_canvas_print_size == 'SRA3':
-                os.replace(filename, input_viz_4_4 + filename)
+                # Всё что идёт в viz_4+4
+                case f_size, f_colorify, f_canvas_print_size \
+                    if f_size in ['89x49', '49x89'] and f_colorify in ['4+4', '1+1'] and f_canvas_print_size == 'SRA3':
+                    os.replace(filename, input_viz_4_4 + filename)
 
-            # Всё что идёт сразу в папку "готово" - output
-            case f_size, f_colorify, f_canvas_print_size \
-                if (f_size in ['487x330', '330x487'] and f_canvas_print_size == 'SRA3+') or \
-                   (f_size in ['450x320', '320x450'] and f_canvas_print_size == 'SRA3'):
-                os.replace(filename, output + filename)
+                # Всё что идёт сразу в папку "готово" - output
+                case f_size, f_colorify, f_canvas_print_size \
+                    if (f_size in ['487x330', '330x487'] and f_canvas_print_size == 'SRA3+') or \
+                       (f_size in ['450x320', '320x450'] and f_canvas_print_size == 'SRA3'):
+                    os.replace(filename, output + filename)
 
-            # Всё что идёт в SRA3_universal
-            case f_size, f_colorify, f_canvas_print_size \
-                if f_size not in ['450x320', '320x450', '487x330', '330x487'] and f_canvas_print_size == 'SRA3':
-                os.replace(filename, input_SRA3 + filename)
+                # Всё что идёт в SRA3_universal
+                case f_size, f_colorify, f_canvas_print_size \
+                    if f_size not in ['450x320', '320x450', '487x330', '330x487'] and f_canvas_print_size == 'SRA3':
+                    os.replace(filename, input_SRA3 + filename)
 
-            # Всё что идёт в SRA3+_universal
-            case f_size, f_colorify, f_canvas_print_size \
-                if f_size not in ['450x320', '320x450', '487x330', '330x487'] and f_canvas_print_size == 'SRA3+':
-                os.replace(filename, input_SRA3_plus + filename)
+                # Всё что идёт в SRA3+_universal
+                case f_size, f_colorify, f_canvas_print_size \
+                    if f_size not in ['450x320', '320x450', '487x330', '330x487'] and f_canvas_print_size == 'SRA3+':
+                    os.replace(filename, input_SRA3_plus + filename)
 
-            # Всё остальное. Wildcard.
-            case _:
-                os.replace(filename, errors + filename)
+                # Всё остальное. Wildcard.
+                case _:
+                    os.replace(filename, errors + filename)
 
-    time.sleep(3)
+        time.sleep(3)
+
+    except:
+        print(f'Не понимаю файл с именем {filename}\n направляю в папку с ошибками')
+        os.replace(filename, errors + filename)
 
 # TODO: ??? 4) Check output hot folder "output" and "errors" and send a message to user how many files are done
 #  len(output) and how many files are invalid len(errors). summ() of this file will == to len(from_path).
